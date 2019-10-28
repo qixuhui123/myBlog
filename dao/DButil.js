098554de21bbe258cc0422a0e52e5da9ec00853c
qixuhui123 <614139493@qq.com>
2019-10-28 10:10:09 +0800

    create file blog

--- dao/DButil.js
+++ dao/DButil.js
@@ -2,7 +2,7 @@ var mysql =  require("mysql");
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host:"127.0.0.1",
+        host:"192.168.56.1",
         port:"3306",
         user: "root",
         password:"Q82004531",
